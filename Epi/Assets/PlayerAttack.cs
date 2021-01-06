58d0c6409c869132c9fcbc4ed1b56bbd93f92e6c
Rena <angelina.colas@epitech.eu>
2021-01-06 14:03:41 +0100

    [ADD]: add fight

---
+++ Epi/Assets/PlayerAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject Hand;
+    public Weapon myWeapon;
+    Animator handAnimation;
+
+    void Start()
+    {
+        handAnimation = Hand.GetComponent<Animator>();
+        myWeapon = Hand.GetComponentInChildren<Weapon>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonUp(0))
+            doAttack();       
+    }
+
+    private void doAttack() {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * myWeapon.attackRange, Color.magenta);
+        if (Physics.Raycast(ray, out hit, myWeapon.attackRange)) {
+            Debug.Log(hit.collider.gameObject.name);
+            if (hit.collider.tag == "Ennemy") {
+                ennemyHealth eHealth = hit.collider.GetComponent<ennemyHealth>();
+                eHealth.TakeDammage(myWeapon.attackDamage);
+            }
+        }
+    }
+}
---
+++ Epi/Assets/ennemyHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ennemyHealth : MonoBehaviour
+{
+    public float Health;
+    //public ParticleEffect prefabParticle;
+
+    //private GameObject instantiatedParticle;
+
+    public void TakeDammage(float dam) {
+        Health -= dam;
+        if (Health <= 0) {
+            Die();
+            print("he died");
+        }
+        //print("ennemy take dammage");
+    }
+
+    private void Die(){
+        //instantiatedParticle = Instantiate(prefabParticle, transform.position, Quaternion.identity);
+
+        //Destroy(instantiatedParticle, 5);
+        Destroy(gameObject);
+
+    }
+}
